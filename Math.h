1a217bcb591145a08d593652e21ca5718f384864
Conner Tenn <connertenn@gmail.com>
2018-05-14 21:33:53 -0400

    Added initial info for optimized algorithm

---
+++ Math.h
@@ -0,0 +1,18 @@
+
+#ifndef _MATH_H_
+#define _MATH_H_
+
+#include <iostream>
+#include <string>
+
+double Accumulator;
+
+void Calc(std::string line)
+{
+	for (int i = 0; i < line.size(); i++)
+	{
+		
+	}
+}
+
+#endif
