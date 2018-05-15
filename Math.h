db1c190a7dd02a5c657f7a273ca8c390756999d2
Conner Tenn <connertenn@gmail.com>
2018-05-15 02:28:40 -0400

    Switched from a doubly linked list to a stack. Optimized push and pop

--- Math.h
+++ Math.h
@@ -12,46 +12,33 @@ struct Elem
 	T Value;
 	
 	Elem<T> *Prev = 0;
-	Elem<T> *Next = 0;
 };
 
 template<typename T>
-struct List
+struct Stack
 {
-	Elem<T> *First = 0;
 	Elem<T> *Last = 0;
-};
-
-template<typename T>
-void Push(List<T> list, T value)
-{
-	Elem<T> *newElem = new Elem<T>;
 	
-	if (!list.First) { list.First = newElem; }
-	if (list.Last) { list.Last->Next = newElem; }
-	newElem->Prev = list.Last;	
-	list.Last = newElem;
-}
-
-template<typename T>
-T Pop(List<T> list)
-{
-	Elem<T> *last = list.Last;
-	if (last)
+	void Push(T value)
 	{
+		Elem<T> *newElem = new Elem<T>;
+		newElem->Prev = Last;
+		Last = newElem;
+	}
+	
+	T Pop()
+	{
+		Elem<T> last = Last;
 		T value = last->Value;
-		list.Last = last->Prev;
-		if (last == list.First) { list.First = 0; }
-		
-		delete last; 
+		Last = last->Prev;
+		delete last;
 		return value;
 	}
-	return 0;
-}
+};
 
 
-List<char> OperatorList;
-List<double> ValueList;
+Stack<char> OperatorStack;
+Stack<double> ValueStack;
 
 double Accumulator = 0;
 int DecimalPlace = -1;
@@ -79,49 +66,50 @@ void Calc(char *line)
 	
 	while (State != END)
 	{
-		if (line[i] != ' ')
+		if (line[i] == ' ')
 		{
-			if (('0' <= line[i] && line[i] <= '9') || line[i] == '.')
-			{
-				State = NUM;
-				//Create a loop in here
-				if (line[i] == '.')
-				{
-					DecimalPlace = 1;
-				}
-				else if (DecimalPlace > 0)
-				{
-					Accumulator+= (line[i]-'0')/pow(10, DecimalPlace);
-				}
-				else
-				{
-					Accumulator *= 10;
-					Accumulator += (line[i] - '0');
-				}
-			}
-			else if (line[i] == '+' || line[i] == '-' || line[i] == '*' || line[i] == '/')
-			{
-				State = OPE;
-			}
-			else if (line[i] == '(')
-			{
-				State = OBR;
-				Push<char>(OperatorList, '(');
-			}
-			else if (line[i] == ')')
-			{
-				State = CBR;
-			}
-			
-			//Move to after loop in number handling section
-			if (PrevState == NUM && PrevState != State)
-			{
-				Push<double>(ValueList, Accumulator);
-				Accumulator = 0;
-			}
-			
-			PrevState = State;
+			i++;
 		}
+		else if (('0' <= line[i] && line[i] <= '9') || line[i] == '.')
+		{
+			State = NUM;
+			//Create a loop in here
+			if (line[i] == '.')
+			{
+				DecimalPlace = 1;
+			}
+			else if (DecimalPlace > 0)
+			{
+				Accumulator+= (line[i]-'0')/pow(10, DecimalPlace);
+			}
+			else
+			{
+				Accumulator *= 10;
+				Accumulator += (line[i] - '0');
+			}
+		}
+		else if (line[i] == '+' || line[i] == '-' || line[i] == '*' || line[i] == '/')
+		{
+			State = OPE;
+		}
+		else if (line[i] == '(')
+		{
+			State = OBR;
+			OperatorStack.Push('(');
+		}
+		else if (line[i] == ')')
+		{
+			State = CBR;
+		}
+		
+		//Move to after loop in number handling section
+		if (PrevState == NUM && PrevState != State)
+		{
+			ValueStack.Push(Accumulator);
+			Accumulator = 0;
+		}
+		
+		PrevState = State;
 	}
 }
 
