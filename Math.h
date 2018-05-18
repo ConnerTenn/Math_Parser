dd1930ba1ea98eb5e61c81d76acf099815fba8ff
Conner Tenn <connertenn@gmail.com>
2018-05-18 09:40:44 -0400

    Optimizations, Fixes, Added power operator

--- Math.h
+++ Math.h
@@ -10,7 +10,6 @@ template<typename T>
 struct Elem
 {
 	T Value;
-	
 	Elem<T> *Prev = 0;
 };
 
@@ -29,75 +28,46 @@ struct Stack
 	
 	T Pop()
 	{
-		if (!Last) { return 0; }
 		Elem<T> *last = Last;
 		T value = last->Value;
 		Last = last->Prev;
 		delete last;
 		return value;
 	}
-	
-	T Back()
-	{
-		return Last->Value;
-	}
 };
-
 
 Stack<char> OperatorStack;
 Stack<double> ValueStack;
 
-enum States
-{
-	NUL,
-	NUM,
-	OPE,
-	OBR,
-	CBR
-};
+enum States { NUL, NUM, OPE, OBR, CBR };
 
 int Eval(char ope)
 {
-	double num1, num2;
+	if (!ValueStack.Last) { return 0; } double num2 = ValueStack.Pop();
+	if (!ValueStack.Last) { return 0; } double num1 = ValueStack.Pop();
 	
 	double result = 0;
-	
-	//std::cout << "ope:" << ope << "\n";
 	
 	switch (ope)
 	{
 	case '+':
-		if (!ValueStack.Last) { return 0; } num2 = ValueStack.Pop();
-		if (!ValueStack.Last) { return 0; } num1 = ValueStack.Pop();
 		result = num1+num2;
 		break;
 	case '-':
-		if (!ValueStack.Last) { return 0; } num2 = ValueStack.Pop();
-		if (!ValueStack.Last) { return 0; } num1 = ValueStack.Pop();
 		result = num1-num2;
 		break;
 	case '*':
-		if (!ValueStack.Last) { return 0; } num2 = ValueStack.Pop();
-		if (!ValueStack.Last) { return 0; } num1 = ValueStack.Pop();
 		result = num1*num2;
 		break;
 	case '/':
-		if (!ValueStack.Last) { return 0; } num2 = ValueStack.Pop();
-		if (!ValueStack.Last) { return 0; } num1 = ValueStack.Pop();
 		result = num1/num2;
 		break;
-	case 'N':
-		if (!ValueStack.Last) { return 0; } num1 = ValueStack.Pop();
-		result = -num1;
-		break;
-	case 'P':
-		if (!ValueStack.Last) { return 0; } num1 = ValueStack.Pop();
-		result = num1;
+	case '^':
+		result = pow(num1,num2);
 		break;
 	}
 	
-	std::cout << "eval:" << num1 << " " << ope << " " << num2 << "\n";
-	
+	std::cout << "eval:" << num1 << " " << ope << " " << num2 << " = " << result << "\n";
 	ValueStack.Push(result);
 	return 1;
 }
@@ -106,9 +76,9 @@ int Eval(char ope)
 int Precidance(char ope)
 {
 	//Priority map for each operator
-	char order[8][2] = { {'(', 0}, {')', 0}, {'+', 1}, {'-', 1}, {'P', 1}, {'N', 1}, {'*', 2}, {'/', 2} };
+	char order[9][2] = { {'(', 0}, {')', 0}, {'+', 1}, {'-', 1}, {'P', 1}, {'N', 1}, {'*', 2}, {'/', 2}, {'^', 3} };
 	
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 9; i++)
 	{
 		if (ope == order[i][0]) { return order[i][1]; }
 	}
@@ -119,56 +89,45 @@ int Precidance(char ope)
 double Calc(char *line, int *report = 0, int *errorPos = 0)
 {
 	bool negative = false;
-	
 	States State = NUL, PrevState = NUL;
 	
+	auto errorFunc = [&](int reportVal, int pos)->int{ if (report) { *report = reportVal; } if (errorPos) { *errorPos = pos; } return 0; };
+	
 	if (report) { *report = 0; }
+	if (errorPos) { *errorPos = 0; }
 	
 	while (ValueStack.Last) { ValueStack.Pop(); }
 	while (OperatorStack.Last) { OperatorStack.Pop(); }
 	
-	//auto gg = 2;
-	auto errorFunc = [&](int reportVal, int pos)->int{ if (report) { *report = reportVal; } if (errorPos) { *errorPos = pos; } return 0; };
-	//[=](int reportVal, int pos)->int{ if (report) { *report = reportVal; } if (errorPos) { *errorPos = pos; } return 0; };
-	//class lambda []int (int reportVal, int pos)->int errorFunc;
-	//lambda 
-	/*struct Functor
-	{
-		int *report, *errorPos;
-		Functor(int *reportIn, int *errorPosIn) : report(reportIn), errorPos(errorPosIn) {} 
-		int operator()(int reportVal, int pos) { if (report) { *report = reportVal; } if (errorPos) { *errorPos = pos; } return 0; }
-	} errorFunc(report, errorPos);*/
-	
 	int i = 0;
 	while (line[i])
 	{
-		if (line[i] == ' ')
-		{
-			i++;
-		}
+		if (line[i] == ' ') { i++; }
 		else if (('0' <= line[i] && line[i] <= '9') || line[i] == '.')
 		{
 			State = NUM;
 			double Accumulator = 0;
 			int DecimalPlace = -1;
+			int numState = 0;
 			
 			if (PrevState == NUM) { return errorFunc(1, i); }
-			//Create a loop in here
+			
 			while (('0' <= line[i] && line[i] <= '9') || line[i] == '.')
 			{
 				if (line[i] == '.')
 				{
-					//fix allowing leading decimal place
-					if (DecimalPlace >= 0) { return errorFunc(1, i); }
-					DecimalPlace = 1;
+					if (numState != 1) { return errorFunc(1, i); }
+					numState = 2;
+					DecimalPlace = 10;
 				}
-				else if (DecimalPlace > 0)
+				else if (numState == 2)
 				{
-					Accumulator+= (line[i]-'0')/pow(10, DecimalPlace);
-					DecimalPlace++;
+					Accumulator += double(line[i]-'0')/DecimalPlace;
+					DecimalPlace *= 10;
 				}
 				else
 				{
+					numState = 1;
 					Accumulator *= 10;
 					Accumulator += (line[i] - '0');
 				}
@@ -180,22 +139,19 @@ double Calc(char *line, int *report = 0, int *errorPos = 0)
 			ValueStack.Push( (negative ? -1 : 1) * Accumulator);
 			negative = false;
 		}
-		else if (line[i] == '+' || line[i] == '-' || line[i] == '*' || line[i] == '/')
-		{	
+		else if (Precidance(line[i] >= 0))
+		{
 			State = OPE;
-			
-			//char ope = line[i];
 			
 			if (line[i] == '-' && PrevState != NUM && PrevState != CBR) { negative = !negative; }
 			else if (line[i] == '+' && PrevState != NUM && PrevState != CBR) { }
 			else
 			{
-				if (PrevState == NUL || PrevState == OPE || PrevState == OBR) { return errorFunc(1, i); }			
+				if (PrevState == NUL || PrevState == OPE || PrevState == OBR) { return errorFunc(1, i); }
 			
-				if (OperatorStack.Last && Precidance(OperatorStack.Back()) >= Precidance(line[i]))
+				while (OperatorStack.Last && Precidance(OperatorStack.Last->Value) >= Precidance(line[i]))
 				{
-					if (!Eval(OperatorStack.Back())) { return errorFunc(2, i); }
-					OperatorStack.Pop();
+					if (!Eval(OperatorStack.Pop())) { return errorFunc(2, i); }
 				}
 				OperatorStack.Push(line[i]);
 			}
@@ -224,6 +180,10 @@ double Calc(char *line, int *report = 0, int *errorPos = 0)
 				ope = OperatorStack.Pop();
 			}
 			i++;
+		}
+		else
+		{
+			return errorFunc(4, i);
 		}
 	
 		PrevState = State;
